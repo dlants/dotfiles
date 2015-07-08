055b75ba85ba39fc4339c3d6427723429fcfd9e5
Denis Lantsman <denis@desmos.com>
2015-07-07 19:29:00 -0700

    moving to fish!

--- init.zsh
+++ init.zsh
@@ -1,15 +1,11 @@
-#! /bin/zsh
+#! /bin/bash
 
 # set up vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# grml zsh config
-wget -O ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-
-ln -sf ~/dotfiles/zshrc.local ~/.zshrc.local
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 vim +PluginInstall +qall
 
 # ctrlp-cmatcher requires an install
-zsh -c "cd ~/.vim/bundle/ctrlp-cmatcher && CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments ./install.sh"
+bash -c "cd ~/.vim/bundle/ctrlp-cmatcher && CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments ./install.sh"
