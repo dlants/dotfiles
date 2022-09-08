f06a9868243efb7e55f0c81251cbb790ae9e7cd3
Denis Lantsman <denis.lantsman@gmail.com>
2022-09-08 16:56:13 -0700

    fixup init script

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 ln -sf ~/src/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/src/dotfiles/zshrc ~/.zshrc
+ln -sf ~/src/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/src/dotfiles/nvim/lua ~/.config/nvim/lua
 
 ln -sf ~/src/dotfiles/scripts/start-tmux /usr/local/bin/start-tmux
 ln -sf ~/src/dotfiles/scripts/tmux-session-using-fzf /usr/local/bin/tmux-session-using-fzf
