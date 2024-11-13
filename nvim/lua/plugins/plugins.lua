6ad99371006e4a109fefa18fdb92ab96cd124276
Denis Lantsman <dlantsman@amplify.com>
2024-11-13 09:58:50 -0800

    add zed keymap

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -582,19 +582,17 @@ return {
           },
           strategies = {
             chat = {
-              adapter = "copilot"
+              adapter = "anthropic"
             },
             inline = {
-              adapter = "copilot"
+              adapter = "anthropic"
             },
             agent = {
-              adapter = "copilot"
+              adapter = "anthropic"
             }
           },
           display = {
-            chat = {
-              show_settings = true
-            }
+            chat = {}
           },
           prompt_library = {
             ["dcgview"] = {
@@ -690,54 +688,55 @@ return {
   --     }
   --   }
   -- },
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   lazy = false,
-  --   version = false, -- set this if you want to always pull the latest change
-  --   opts = {},
-  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   build = "make",
-  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --     -- {
-  --     --   -- support for image pasting
-  --     --   "HakonHarnes/img-clip.nvim",
-  --     --   event = "VeryLazy",
-  --     --   opts = {
-  --     --     -- recommended settings
-  --     --     default = {
-  --     --       embed_image_as_base64 = false,
-  --     --       prompt_for_file_name = false,
-  --     --       drag_and_drop = {
-  --     --         insert_mode = true
-  --     --       },
-  --     --       -- required for Windows users
-  --     --       use_absolute_path = true
-  --     --     }
-  --     --   }
-  --     -- },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = {"markdown", "Avante"}
-  --       },
-  --       ft = {"markdown", "Avante"}
-  --     }
-  --   },
-  --   opts = {
-  --     provider = "copilot",
-  --     auto_suggestions_provider = "copilot",
-  --     system_prompt = llm_default_prompt
-  --   }
-  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    opts = {
+      provider = "anthropic",
+      auto_suggestions_provider = "anthropic",
+      system_prompt = llm_default_prompt
+    }, -- set this if you want to always pull the latest change
+    opts = {},
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      -- below are optional
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      -- {
+      --   -- support for image pasting
+      --   "HakonHarnes/img-clip.nvim",
+      --   event = "VeryLazy",
+      --   opts = {
+      --     -- recommended settings
+      --     default = {
+      --       embed_image_as_base64 = false,
+      --       prompt_for_file_name = false,
+      --       drag_and_drop = {
+      --         insert_mode = true
+      --       },
+      --       -- required for Windows users
+      --       use_absolute_path = true
+      --     }
+      --   }
+      -- },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = {"markdown", "Avante"}
+        },
+        ft = {"markdown", "Avante"}
+      }
+    },
+    version = false
+  },
   {
     "ggandor/leap.nvim",
     config = function()
