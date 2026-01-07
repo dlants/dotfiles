52d1f78336583057b9ca9f0a125d789ed961facd
Denis Lantsman <denis.lantsman@benchling.com>
2026-01-07 11:41:40 -0800

    fixup rustup/fish interaction

--- nix/common.nix
+++ nix/common.nix
@@ -82,6 +82,9 @@
   # Magenta skills symlinks (individual per skill)
   home.file.".magenta/skills/browser".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/magenta-skills/browser";
 
+  # Prevent rustup from creating a broken fish config (nix manages PATH)
+  home.file.".config/fish/conf.d/rustup.fish".text = "";
+
   # Clone magenta.nvim if it doesn't exist
   home.activation.cloneMagenta = lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [ ! -d "$HOME/src/magenta.nvim" ]; then
