9d6eab2b98e7e50439acebe34998c1c0aeeb3b4b
Denis Lantsman <denis.lantsman@benchling.com>
2026-01-16 08:52:46 -0800

    set up pkgx on linux

--- nix/linux.nix
+++ nix/linux.nix
@@ -9,6 +9,13 @@
     wget
   ];
 
+  # Install pkgx via curl (brew not available on Linux)
+  home.activation.pkgxInstall = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if ! command -v pkgx &> /dev/null; then
+      curl -fsS https://pkgx.sh | sh
+    fi
+  '';
+
   # Fish config (Linux-specific)
   xdg.configFile."fish/config.fish".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/fish/config-linux.fish");
 }
