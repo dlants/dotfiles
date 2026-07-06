47fe769be7ed987111b9f9153d4ce67d8d97ca9b
Denis Lantsman <denis.lantsman@benchling.com>
2026-07-06 11:35:24 -0700

    zsh+tmux: C-j/k/l for shell history in insert mode, pane nav in normal mode

--- zsh/config-shared.zsh
+++ zsh/config-shared.zsh
@@ -4,11 +4,32 @@
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
-# <C-l> accepts the whole zsh-autosuggestion (grayed-out preview);
-# <C-j>/<C-k> mirror the down/up arrows (history-substring-search).
+# tmux forwards C-h/j/k/l to interactive zsh panes (see tmux.conf is_vim, which
+# also matches zsh). We mirror vim-tmux-navigator: in INSERT mode C-j/C-k/C-l
+# drive history-search / autosuggestion accept; in NORMAL (vicmd) mode
+# C-h/j/k/l switch tmux panes (Esc first to navigate away from the shell).
+if [[ -n $TMUX ]]; then
+  _zsh_tmux_pane_left()  { tmux select-pane -L }
+  _zsh_tmux_pane_down()  { tmux select-pane -D }
+  _zsh_tmux_pane_up()    { tmux select-pane -U }
+  _zsh_tmux_pane_right() { tmux select-pane -R }
+  zle -N _zsh_tmux_pane_left
+  zle -N _zsh_tmux_pane_down
+  zle -N _zsh_tmux_pane_up
+  zle -N _zsh_tmux_pane_right
+fi
+
 # zsh-vi-mode resets bindkeys on init, so (re)bind after it loads.
 zvm_after_init_commands+=(
-  'bindkey "^L" autosuggest-accept'
-  'bindkey "^K" history-substring-search-up'
-  'bindkey "^J" history-substring-search-down'
+  'bindkey -M viins "^L" autosuggest-accept'
+  'bindkey -M viins "^K" history-substring-search-up'
+  'bindkey -M viins "^J" history-substring-search-down'
 )
+if [[ -n $TMUX ]]; then
+  zvm_after_init_commands+=(
+    'bindkey -M vicmd "^H" _zsh_tmux_pane_left'
+    'bindkey -M vicmd "^J" _zsh_tmux_pane_down'
+    'bindkey -M vicmd "^K" _zsh_tmux_pane_up'
+    'bindkey -M vicmd "^L" _zsh_tmux_pane_right'
+  )
+fi
